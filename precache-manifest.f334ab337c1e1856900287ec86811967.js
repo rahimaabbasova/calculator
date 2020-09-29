c9e70e47534b41016714e635bfbaeaaeb2dca946
rahima <abbasova.rahima.n@gmail.com>
2020-09-29 16:44:00 +0400

    Updates

---
+++ precache-manifest.f334ab337c1e1856900287ec86811967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8802bedafe3a914084fca452b7a74a7",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "4420391285624365f8d3",
+    "url": "/calculator/static/css/main.e9a19c10.chunk.css"
+  },
+  {
+    "revision": "8f3b4ca9a071031e5a7a",
+    "url": "/calculator/static/js/2.897383d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculator/static/js/2.897383d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4420391285624365f8d3",
+    "url": "/calculator/static/js/main.e3e76d4e.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
